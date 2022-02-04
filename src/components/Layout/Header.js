dce3e7c53cd448a8670453ff1cf386ce10f99a12
Annon007 <62867777+Annon007@users.noreply.github.com>
2022-02-05 04:13:01 +0600

    Header Button Cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import meals from "../../assests/meals.jpg"
-import styles from "./header.module.css"
+import styles from "./header.module.css";
+
+import HeaderButtonCart from "./HeaderButtonCart";
+
 const Header = () => {
     return <>
         <header className={styles.header}>
             <h1>KhudaLagse</h1>
-            <button>Cart</button>
+            <HeaderButtonCart />
         </header>
         <div className={styles.mainImage}>
             <img src={meals} />
---
+++ src/components/Layout/HeaderButtonCart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./headerButtonCart.module.css"
+import { Cart } from "../../assests/icons"
+const HeaderButtonCart = props => {
+    return <button className={styles.button}>
+        <span className={styles.icon}>{Cart}</span>
+        <span>Your Cart</span>
+        <span className={styles.badge}>3</span>
+    </button>
+}
+
+export default HeaderButtonCart;
