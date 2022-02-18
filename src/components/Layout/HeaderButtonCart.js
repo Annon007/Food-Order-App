287ea9e8331a92e6d966846a4cf07cd5c768fd8a
Annon007 <62867777+Annon007@users.noreply.github.com>
2022-02-19 03:26:02 +0600

    button annimation added

--- src/components/Layout/HeaderButtonCart.js
+++ src/components/Layout/HeaderButtonCart.js
@@ -1,14 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./headerButtonCart.module.css";
 import { Cart } from "../../assests/icons";
 
 import CartContext from "../../store/cart-contaxt";
 const HeaderButtonCart = props => {
+    const [btnHighlight, setBtnHighlight] = useState(false);
     const cartContext = useContext(CartContext);
     const numberOfMeals = cartContext.item.reduce((acc, cur) => {
         return acc + cur.amount;
-    }, 0)
-    return <button onClick={() => props.onShowCart()} className={styles.button}>
+    }, 0);
+    const { item } = cartContext;
+    const btnAnnimation = `${styles.button} ${btnHighlight ? styles.bump : ""}`
+    useEffect(() => {
+        if (item.length === 0) return;
+        setBtnHighlight(true);
+        const timer = setTimeout(() => {
+            setBtnHighlight(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [item])
+    return <button onClick={() => props.onShowCart()} className={btnAnnimation}>
         <span className={styles.icon}>{Cart}</span>
         <span>Your Cart</span>
         <span className={styles.badge}>{numberOfMeals}</span>
