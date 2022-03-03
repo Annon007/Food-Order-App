a40adf09429bd39e34c2fae6446f9a060ee6205c
Annon007 <62867777+Annon007@users.noreply.github.com>
2022-03-03 21:19:53 +0600

    fetching from fireBase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,35 +1,47 @@
+import { useEffect, useState } from "react";
 import styles from "./availableMeals.module.css";
 import Card from "../UI/Card";
 import MealItems from "./MealItems/MealItems";
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+
 
 const AvailableMeals = () => {
-    const mealLists = DUMMY_MEALS.map(meals => {
+    const [transformedData, setTransformedData] = useState();
+    const [isLodaing, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+    useEffect(() => {
+        const getMeals = async () => {
+            try {
+                setIsLoading(true);
+                const sendReq = fetch("https://react-prac-adae1-default-rtdb.firebaseio.com/meals.json");
+
+                const res = await sendReq;
+                if (!res.ok) {
+
+                    throw new Error("Something Went Wrong!");
+                }
+                const data = await res.json();
+                let fetchedData = [];
+
+                for (const meal in data) {
+                    fetchedData.push({
+                        id: meal,
+                        name: data[meal].name,
+                        description: data[meal].description,
+                        price: data[meal].price
+                    })
+                };
+                setTransformedData(fetchedData)
+                setIsLoading(false);
+                setIsError();
+
+            } catch (err) {
+                setIsLoading(false)
+                setIsError(err);
+            }
+        }
+        getMeals()
+    }, [])
+    const mealLists = transformedData?.map(meals => {
         return <MealItems
             key={meals.id}
             id={meals.id}
@@ -39,7 +51,13 @@ const AvailableMeals = () => {
         />
     });
 
+    if (isLodaing) {
+        return <p className={styles.loadingText}>Loading...</p>
+    }
 
+    if (isError) {
+        return <p className={styles.errorTxt}>Fetching Filed!</p>
+    }
     return <section className={styles.meals}>
         <Card>
             <ul >
